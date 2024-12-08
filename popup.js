302c15e4388796848a5de3ee68e3e8aff8b56ca0
rohit <=>
2024-12-08 18:09:24 +0530

    Youtube PIP Extension

---
+++ popup.js
@@ -0,0 +1,21 @@
+// popup.js
+document.getElementById('add-id-button').addEventListener('click', () => {
+    // Query for the active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: addVideoId
+        });
+    });
+});
+
+function addVideoId() {
+    console.log('Running addVideoId() function');
+    const videoElement = document.querySelector("#movie_player .html5-main-video");
+    if (videoElement) {
+        videoElement.requestPictureInPicture()
+        console.log('Video Element Found');
+    } else {
+        console.log('No video element found on this page.');
+    }
+}
